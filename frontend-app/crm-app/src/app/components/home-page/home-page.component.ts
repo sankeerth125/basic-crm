3fc8cb55769c9b8d608b9f4c101dd54a48443a33
System Administrator <root@Sankeerths-MacBook-Air.local>
2023-05-10 14:35:41 +0530

    renamed folders

---
+++ frontend-app/crm-app/src/app/components/home-page/home-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page',
+  standalone: true,
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss']
+})
+export class HomePageComponent {
+
+}
